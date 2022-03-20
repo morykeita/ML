1ded920c39ad685d11cf97ea52842961a80e58b7
morykeita <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>
2022-03-20 13:58:08 -0400

    first commit

--- src/create_folds.py
+++ src/create_folds.py
@@ -3,3 +3,8 @@ from sklearn import model_selection
 
 if __name__ == '__main___':
     df = pd.read_csv('input/train.csv')
+    df['kfold'] = -1
+
+    df = df.sample(frac=1).reset_index(drop=True)
+    
+    kf = model_selection.StratifiedKFold(n_splits=5,shuffle=False, random_state=None)
