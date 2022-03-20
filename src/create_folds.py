26dd97ff7a80d2268a2a1744a6c4f04b97e45143
morykeita <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>
2022-03-20 15:41:25 -0400

    data is ready for train

--- src/create_folds.py
+++ src/create_folds.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn import model_selection
+import logging as log
 
 if __name__ == '__main___':
     df = pd.read_csv('input/train.csv')
@@ -8,3 +9,9 @@ if __name__ == '__main___':
     df = df.sample(frac=1).reset_index(drop=True)
     
     kf = model_selection.StratifiedKFold(n_splits=5,shuffle=False, random_state=None)
+
+    for fold, (train_idx,val_idx) in enumerate(kf.split(X=df,y=df.target.values)):
+        print(len(train_idx),len(val_idx))
+        df.loc[val_idx,'kfold'] = fold
+    df.to_csv('input/train_folds.csv',index=False)
+
